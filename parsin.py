d9d7eaa2db2d41ca1cd6729fe7b475493b6dd15d
DeNiSsca911 <denwertyu@gmail.com>
2025-05-23 20:50:11 +0300

    commit 1 file to mane

--- parsin.py
+++ parsin.py
@@ -3,3 +3,7 @@ import requests
 response = requests.get('https://www.google.com/')
 
 print(response.status_code)
+if response.ok:
+    print("запрос успешно выполнен")
+else:
+    print("произошла ошибка")
